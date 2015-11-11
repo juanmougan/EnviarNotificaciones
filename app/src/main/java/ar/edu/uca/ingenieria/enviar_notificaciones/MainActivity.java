d2b95e917e307c7240a7cca25f620899895d5aa1
Juan Manuel Mougan <juanmougan@gmail.com>
2015-11-11 20:34:10 -0300

    Hardcoded data on spinner

--- app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/MainActivity.java
+++ app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +14,19 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadSubscriptionListSpinner();
     }
 
+    private void loadSubscriptionListSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.subscription_list_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.mock_sl, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
