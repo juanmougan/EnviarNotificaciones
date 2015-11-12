dc8c2b38ed38e536068036bc9e88d77c0c6f2540
Juan Manuel Mougan <jmougan@hexacta.com>
2015-11-12 15:17:57 -0300

    Started with the service layer

--- app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/service/MessageService.java
+++ app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/service/MessageService.java
@@ -1,9 +1,18 @@
 package ar.edu.uca.ingenieria.enviar_notificaciones.service;
 
+import ar.edu.uca.ingenieria.enviar_notificaciones.model.Message;
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.POST;
+
 /**
  * Interface for the Message service
  *
  * Created by juanmougan@gmail.com on 10/11/15.
  */
 public interface MessageService {
+
+    @POST("/notifications")
+    Response sendMessage(@Body Message message);
+
 }
--- app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/service/SubscriptionListService.java
+++ app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/service/SubscriptionListService.java
@@ -1,9 +1,17 @@
 package ar.edu.uca.ingenieria.enviar_notificaciones.service;
 
+import ar.edu.uca.ingenieria.enviar_notificaciones.model.SubscriptionList;
+import retrofit.Callback;
+import retrofit.http.GET;
+
 /**
  * Interface for the SubscriptionList service
  *
  * Created by juanmougan@gmail.com on 10/11/15.
  */
 public interface SubscriptionListService {
+
+    @GET("/subscriptionlists")
+    SubscriptionList getSubscriptionLists();
+
 }
