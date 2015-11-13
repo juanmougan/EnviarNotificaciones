b105c2d8d1dade3d87ecb858c6ba27d3d9614b8b
Juan Manuel Mougan <juanmougan@gmail.com>
2015-11-13 20:21:33 -0300

    Filled the class attributes

--- app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/model/Message.java
+++ app/src/main/java/ar/edu/uca/ingenieria/enviar_notificaciones/model/Message.java
@@ -6,4 +6,46 @@ package ar.edu.uca.ingenieria.enviar_notificaciones.model;
  * Created by juanmougan@gmail.com on 10/11/15.
  */
 public class Message {
+
+    private String title;
+
+    private String message;
+
+    private SubscriptionList deliverToList;
+
+    public Message(String title, String message, SubscriptionList deliverToList) {
+        this.title = title;
+        this.message = message;
+        this.deliverToList = deliverToList;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public SubscriptionList getDeliverToList() {
+        return deliverToList;
+    }
+
+    public void setDeliverToList(SubscriptionList deliverToList) {
+        this.deliverToList = deliverToList;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + this.title + "\tMessage: " + this.message + "\tDeliver to: "
+                + this.deliverToList.toString();
+    }
 }
